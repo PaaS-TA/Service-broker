5cfd73d9cb5bc02c46fee9a9b4ffd8fc72402a68
unknown <whalsrn0710@bluedigm.com>
2015-06-19 16:26:17 +0900

    Initial commit

---
+++ openpaas-cf-service-java-broker-cubrid/src/main/java/org/openpaas/servicebroker/cubrid/service/impl/CubridAdminService.java
@@ -0,0 +1,167 @@
+package org.openpaas.servicebroker.cubrid.service.impl;
+
+import org.openpaas.servicebroker.cubrid.exception.CubridServiceException;
+import org.openpaas.servicebroker.model.ServiceInstance;
+import org.openpaas.servicebroker.model.ServiceInstanceBinding;
+import org.openpaas.servicebroker.util.JSchUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Service;
+
+/**
+ * Utility class for manipulating a Cubrid database.
+ * 
+ * @author 
+ *
+ */
+@Service
+@PropertySource("classpath:datasource.properties")
+public class CubridAdminService {
+
+	
+	private Logger logger = LoggerFactory.getLogger(CubridAdminService.class);
+	
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private JSchUtil jsch;
+	
+	@Autowired
+	public CubridAdminService(JSchUtil jsch) {
+		this.jsch = jsch;
+	}
+		
+	public boolean isExistsService(ServiceInstance instance){
+		return true;
+	}
+	
+	public boolean isExistsUser(String userId){
+		return true;
+	}
+	
+	public ServiceInstance findById(String id){
+		return null;
+	}
+	
+	public ServiceInstanceBinding findBindById(String id){
+		return null;
+	}
+	
+	public void delete(String id) throws CubridServiceException{
+		try{
+			
+		} catch (Exception e) {
+			throw handleException(e);
+		}
+	}
+	
+	public void deleteBind(String id) throws CubridServiceException{
+		try{
+			
+		} catch (Exception e) {
+			throw handleException(e);
+		}
+	}
+	
+	public void save(ServiceInstance serviceInstance) throws CubridServiceException{
+		try{
+			
+		} catch (Exception e) {
+			throw handleException(e);
+		}
+	}
+	
+	public void saveBind(ServiceInstanceBinding serviceInstanceBinding) throws CubridServiceException{
+		try{
+			
+		} catch (Exception e) {
+			throw handleException(e);
+		}
+	}
+	
+	public void deleteDatabase(ServiceInstance serviceInstance) throws CubridServiceException{
+		try{
+			
+		} catch (Exception e) {
+			throw handleException(e);
+		}
+	}
+	
+	public void createDatabase(ServiceInstance serviceInstance) throws CubridServiceException{
+		try{
+			
+		} catch (Exception e) {
+			throw handleException(e);
+		}
+	}
+	
+	public void createUser(String database, String userId, String password) throws CubridServiceException{
+		try{
+			String q = "create user " + userId + " password '" + password + "'";
+			String command = "csql -c \""+q+"\" " + database + " -u dba";
+			
+			jsch.shell(command);
+			
+		} catch (Exception e) {
+			throw handleException(e);
+		}
+	}
+	
+	public void deleteUser(String database, String userId) throws CubridServiceException{
+		try{
+			String q = "DROP USER " + userId;
+			String command = "csql -c \""+q+"\" " + database + " -u dba";
+			
+			jsch.shell(command);
+			
+		} catch (Exception e) {
+			throw handleException(e);
+		}
+	}
+	
+	public String getConnectionString(String database, String username, String password) {
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append("jdbc:cubrid:");
+		builder.append(getServerAddresses());
+		builder.append(database);
+		builder.append(":");
+		builder.append(username);
+		builder.append(":");
+		builder.append(password);
+		builder.append(":");
+		
+		return builder.toString();
+	}
+	
+	public String getServerAddresses() {
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append(jsch.getHostname());
+		builder.append(":");
+		builder.append("30000");
+		builder.append(":");
+		return builder.toString();
+	}
+	
+	private CubridServiceException handleException(Exception e) {
+		logger.warn(e.getLocalizedMessage(), e);
+		return new CubridServiceException(e.getLocalizedMessage());
+	}
+	/*
+	private JdbcTemplate getJdbcTemplate(String database) {
+		JdbcTemplate jdbcTamplate = new JdbcTemplate();
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getRequiredProperty("jdbc.driver"));
+		dataSource.setUrl(getConnectionString(database, env.getRequiredProperty("cubrid.username"), env.getRequiredProperty("cubrid.password")));
+		
+		return jdbcTamplate;
+	}*/
+}
---
+++ openpaas-cf-service-java-broker-cubrid/src/test/java/org/openpaas/servicebroker/cubrid/service/impl/CubridAdminServiceTest.java
@@ -0,0 +1,70 @@
+package org.openpaas.servicebroker.cubrid.service.impl;
+
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openpaas.servicebroker.cubrid.CubridConfiguration;
+/*import org.openpaas.servicebroker.cubrid.config.Application;
+import org.openpaas.servicebroker.cubrid.config.BrokerConfig;
+import org.openpaas.servicebroker.cubrid.config.CatalogConfig;
+import org.openpaas.servicebroker.cubrid.config.CubridConfig;*/
+import org.openpaas.servicebroker.cubrid.exception.CubridServiceException;
+import org.openpaas.servicebroker.util.JSchUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CubridConfiguration.class)
+public class CubridAdminServiceTest {
+	
+	@Autowired
+	private JSchUtil jsch;
+
+	private CubridAdminService cubridAdminService;
+
+	private String database;
+	private String username;
+	private String password;
+	
+
+	@Before
+	public void initialize() {
+		jsch.enableDebug();
+		cubridAdminService = new CubridAdminService(jsch);
+		
+		database = "testdb";
+		username = "testuser";
+		password = "testpass";
+	}
+	
+	@Test
+	public void createUser() throws CubridServiceException {
+
+		cubridAdminService.createUser("testdb", "testuser2", "testpass2");
+	}
+	
+	@Test
+	public void deleteUser() throws CubridServiceException {
+
+		cubridAdminService.deleteUser("testdb", "testuser2");
+	}
+	
+	@Test
+	public void getConnectionString() {
+		String expected = "jdbc:cubrid:localhost:30000:"+database+":"+username+":"+password+":";
+		
+		assertEquals(expected, cubridAdminService.getConnectionString(database, username, password));
+		
+		
+		
+	}
+
+}
