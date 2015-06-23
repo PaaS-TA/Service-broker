171ce5785c24700e1dcaa17f296af866920587c6
kimdojun <kimdojun@bluedigm.com>
2015-06-23 14:23:06 +0900

    익셉션추가, 클래스 삭제
    카탈로그 서비스에 익셉션 추가
    BeanCatalogService,  BeanCatalogServiceTest 클래스 삭제

--- openpaas-service-java-broker/src/main/java/org/openpaas/servicebroker/service/CatalogService.java
+++ openpaas-service-java-broker/src/main/java/org/openpaas/servicebroker/service/CatalogService.java
@@ -1,5 +1,6 @@
 package org.openpaas.servicebroker.service;
 
+import org.openpaas.servicebroker.exception.ServiceBrokerException;
 import org.openpaas.servicebroker.model.Catalog;
 import org.openpaas.servicebroker.model.ServiceDefinition;
 
@@ -15,12 +16,12 @@ public interface CatalogService {
 	/**
 	 * @return The catalog of services provided by this broker.
 	 */
-	Catalog getCatalog();
+	Catalog getCatalog() throws ServiceBrokerException;
 
 	/**
 	 * @param serviceId  The id of the service in the catalog
 	 * @return The service definition or null if it doesn't exist
 	 */
-	ServiceDefinition getServiceDefinition(String serviceId);
+	ServiceDefinition getServiceDefinition(String serviceId) throws ServiceBrokerException;
 	
 }
