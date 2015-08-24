39a523c60faf4be90b755a90b0f22d31734097ed
kimdojun <kimdojun@bluedigm.com>
2015-08-24 12:58:23 +0900

    코어 버전 업데이트

---
+++ openpaas-service-java-broker/src/main/java/org/openpaas/servicebroker/exception/ServiceDefinitionDoesNotExistException.java
@@ -0,0 +1,17 @@
+package org.openpaas.servicebroker.exception;
+
+/**
+ * Exception denoting an unknown ServiceDefintion
+ * 
+ * @author sgreenberg@gopivotal.com
+ *
+ */
+public class ServiceDefinitionDoesNotExistException extends Exception {
+	
+	private static final long serialVersionUID = -62090827040416788L;
+
+	public ServiceDefinitionDoesNotExistException(String serviceDefinitionId) {
+		super("ServiceDefinition does not exist: id = " + serviceDefinitionId);
+	}
+	
+}
