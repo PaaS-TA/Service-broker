c76b72fafdb92585aedeeded34ef0016c774d732
bd-mingu <whalsrn0710@bluedigm.com>
2015-11-10 11:00:34 +0900

    credentials 항목 수정

--- openpaas-service-java-broker-mongo/src/main/java/org/openpaas/servicebroker/mongodb/service/impl/MongoServiceInstanceBindingService.java
+++ openpaas-service-java-broker-mongo/src/main/java/org/openpaas/servicebroker/mongodb/service/impl/MongoServiceInstanceBindingService.java
@@ -16,6 +16,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mongodb.MongoClientURI;
+
 /**
  * Mongo impl to bind services.  Binding a service does the following:
  * creates a new user in the database (currently uses a default pwd of "password"),
