01bd919b9f4b169a1b727ea8ea48ac3e876f7a58
mingu <whalsrn0710@bluedigm.com>
2016-11-30 16:09:31 +0900

    v2.0 ~ v3.9 사용할 수 있게 수정

--- openpaas-service-java-broker/src/main/java/org/openpaas/servicebroker/config/ServiceBrokerAutoConfiguration.java
+++ openpaas-service-java-broker/src/main/java/org/openpaas/servicebroker/config/ServiceBrokerAutoConfiguration.java
@@ -27,6 +27,6 @@ public class ServiceBrokerAutoConfiguration {
 		//example
 		//"2.5"
 		//"2.5, 2.6, 2.7"
-		return new BrokerApiVersion("2.5, 2.7, 2.6, 2.8");
+		return new BrokerApiVersion("2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9");
 	}
 }
