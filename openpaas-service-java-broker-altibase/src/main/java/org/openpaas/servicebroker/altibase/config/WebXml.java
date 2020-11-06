c29124ac97cb7436cf6429631bdc5f59ced1cd86
YoungheeBae <grace.bae@altibase.com>
2020-11-06 14:44:30 +0900

    'update'

--- openpaas-service-java-broker-altibase/src/main/java/org/openpaas/servicebroker/altibase/config/WebXml.java
+++ openpaas-service-java-broker-altibase/src/main/java/org/openpaas/servicebroker/altibase/config/WebXml.java
@@ -1,8 +1,7 @@
 package org.openpaas.servicebroker.altibase.config;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class WebXml extends SpringBootServletInitializer {
 	
